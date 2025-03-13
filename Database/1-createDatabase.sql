e96d9e59313bb49c03eaef3961d19a67c27a1c97
masoud533 <mapar10278@gmail.com>
2025-03-13 14:22:36 +0330

    complet DataPreprocessing and Statistical Analysis

--- Database/1-createDatabase.sql
+++ Database/1-createDatabase.sql
@@ -6,3 +6,10 @@ CREATE TABLE news (
     date TEXT,
     label TEXT -- 'Fake' or 'Real'
 );
+
+CREATE TABLE cleanedText (
+    id INTEGER,
+    cleaned_text TEXT,
+    label TEXT,
+    FOREIGN KEY(id) REFERENCES news(id)
+);
