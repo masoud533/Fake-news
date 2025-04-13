795cc9d1c627d9edd30e83cd4b999a78651712a9
masoud533 <mapar10278@gmail.com>
2025-04-13 21:07:33 +0330

    update tkinter mini app

--- app/FakeNewsDetector.py
+++ app/FakeNewsDetector.py
@@ -4,7 +4,6 @@ import nltk
 import json
 import pickle
 import tkinter
-import sqlite3
 import numpy as np
 from tkinter import *
 from nltk.corpus import stopwords
@@ -22,14 +21,37 @@ model = open(modelPath, 'rb')
 V = pickle.load(model)
 model.close()
 
-stopWords = nltk.corpus.stopwords.words('english')
+# 2. Initialize stopwords, stemmer, and lemmatizer
+stop_words = set(stopwords.words("english"))
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
 
 
+def predict_class(text):
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    words = word_tokenize(text)
+    words = [word for word in words if word not in stop_words]
+    words = [lemmatizer.lemmatize(word) for word in words]
+    X = [" ".join(words)]
+    x_v = V.transform(X)
+    p = LR.predict(x_v)
+    print(p)
+    return 1
 
 
+def send():
+    msg = EntryBox.get("1.0", "end-1c").strip()
+    
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        res = predict_class(msg)
+        ChatLog.insert(END, "Result: " + res + '\n')
+        ChatLog.config(state=DISABLED)
+        ChatLog.YView(END)
 
 base = Tk()
-base.title("Hello")
+base.title("Fake News Detector")
 base.geometry("450x550")
 base.resizable(width=True, height=True)
 
@@ -40,11 +62,16 @@ ChatLog.config(state=DISABLED)
 scrollbar = Scrollbar(base, command=ChatLog.yview)
 ChatLog['yscrollcommand'] = scrollbar.set
 
-#SendButton = Button(base, font=("Verdana", 12, "bold"), text="Send", width="12",height=5,
-#                    bd=0, bg='#32de97', activebackground='#3c9d9b', fg='#ffffff', command= send)
+SendButton = Button(base, font=("Verdana", 12, "bold"), text="Run", width="12",height=5,
+                    bd=0, bg='#32de97', activebackground='#3c9d9b', fg='#ffffff', command= send)
 
-#EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
-#EntryBox.bind_all("<Key>", _onKeyRelease, "+")
+EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
+scrollbar.place(x=376, y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+base.mainloop()
 
 
 
