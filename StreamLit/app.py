8fbdc73084fce6ecfd85d0213431481d79471c43
masoud533 <mapar10278@gmail.com>
2025-04-17 09:58:28 +0330

    modified StreamLit app and some change to get better the project

--- StreamLit/app.py
+++ StreamLit/app.py
@@ -1,7 +1,38 @@
 import streamlit as st
+import os
+import re
+import nltk
+import pickle
+import tkinter as tk
+from tkinter import *
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+# Load models
+with open(os.path.join('..', 'Models', 'lrModel.model'), 'rb') as model_file:
+    LR = pickle.load(model_file)
+
+with open(os.path.join('..', 'Models', 'tfidf_vectorizer.model'), 'rb') as vectorizer_file:
+    V = pickle.load(vectorizer_file)
+
+stop_words = set(stopwords.words("english"))
+lemmatizer = WordNetLemmatizer()
 
 st.title('Fake or Real News Detection with AI!')
 
 text = st.text_area("Enter your news text: ")
 
 btn = st.button("Detect")
+
+if btn:
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    words = word_tokenize(text)
+    words = [word for word in words if word not in stop_words]
+    words = [lemmatizer.lemmatize(word) for word in words]
+    X = [" ".join(words)]
+    x_v = V.transform(X)
+    p = LR.predict(x_v)[0]
+    result =  "Fake" if p == 0 else "Real"
+    st.success("This News is: " + result)
